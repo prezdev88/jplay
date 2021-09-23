4a18c96b2898e377634cc5a0175a3ecec1b84d1d
prez <patodeath@gmail.com>
2021-09-23 03:01:35 -0300

    Create a pom.xml in root Modify run.sh Delete compile.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
-# si quiere compilar --> sh run.sh -c
-
-if [ "$1" = "-h" ]; then
-    echo "Sólo ejecutar: ./run.sh"
-    echo "Ejecutar y compilar: ./run.sh -c"
-fi
-
-if [ "$1" = "-c" ]; then
-    sh compile.sh
-fi
-
-if [ "$1" != "-h" ]; then
-    cd xjplay
-    java -jar -Dsun.java2d.opengl=true target/xjplay-1.0-SNAPSHOT-jar-with-dependencies.jar
-fi
-
-
+mvn clean
+mvn package
+cd xjplay
+mvn exec:java
