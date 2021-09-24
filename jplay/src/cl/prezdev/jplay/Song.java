ec655a62b3c344a5f401d0bc1ba1cf1cda011a8a
prez <patodeath@gmail.com>
2021-09-24 00:47:43 -0300

    Re order jplay and jplay-common packages Fix all jplay-common sonar lint issues

--- jplay/src/cl/prezdev/jplay/Song.java
+++ jplay/src/cl/prezdev/jplay/Song.java
@@ -1,12 +1,15 @@
 package cl.prezdev.jplay;
 
 import java.io.File;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class Song extends File {
 
     private static final Logger LOGGER = Logger.getLogger("cl.prezdev.jplay.Song");
@@ -50,5 +53,19 @@ public class Song extends File {
 
     public boolean hasPlays() {
         return (this.getPlayCount() != 0);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Song)) return false;
+        if (!super.equals(o)) return false;
+        Song song = (Song) o;
+        return getPlayCount() == song.getPlayCount() && Objects.equals(getName(), song.getName()) && Objects.equals(getAuthor(), song.getAuthor()) && Objects.equals(getAlbum(), song.getAlbum()) && Objects.equals(getYear(), song.getYear()) && Objects.equals(getTrackNumber(), song.getTrackNumber()) && Objects.equals(getMicroseconds(), song.getMicroseconds());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getName(), getAuthor(), getAlbum(), getYear(), getTrackNumber(), getMicroseconds(), getPlayCount());
     }
 }
